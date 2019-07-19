f3f334e70e78b91f6a19eca7a5b4a16d4c23e0ec
Jack Pritchard <jack@noface.co.uk>
2019-07-19 12:39:53 +0100

    init repo

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const request = require("request");
+const cheerio = require("cheerio");
+const app = express();
+
+app.get("/", (req, res) => {
+	if (!req.query.type) throw new Error("No type provided");
+
+	let type = req.query.type.toLowerCase();
+
+	var url;
+
+	if (type === "tech") url = "https://www.etsy.com/uk/featured/tech-trends-uk";
+
+	// The structure of our request call
+	// The first parameter is our URL
+	// The callback function takes 3 parameters, an error, response status code and the html
+	request(url, (error, response, html) => {
+		// First we'll check to make sure no errors occurred when making the request
+		if (!error) {
+			// Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+			var $ = cheerio.load(html);
+
+			// Finally, we'll define the variable we're going to capture
+			// We'll be using Cheerio's function to single out the necessary information
+			// using DOM selectors which are normally found in CSS.
+
+			if (type === "tech") {
+				const products = $(`a[href*="etsy.com/uk/listing"]`);
+				var prediction = $("div.daily-horoscope > p").text();
+				const foundProducts = [];
+
+				$(products).each(function(i, headline) {
+					let product = cheerio.load(this);
+					let title = product(`h2`).text();
+					title = title.replace(/(\r\n|\n|\r)/gm, "");
+					title = title.trim();
+
+					let price =
+						product(`.currency-symbol`).text() +
+						product(`.currency-value`).text();
+
+					if (title !== "" && price !== "") {
+						foundProducts.push({
+							price,
+							title
+						});
+					}
+				});
+
+				// Send the JSON as a response to the client
+				res.send(foundProducts);
+			}
+		}
+	});
+});
+
+app.listen("8080");
+
+console.log("API is running on http://localhost:8080");
+
+module.exports = app;
